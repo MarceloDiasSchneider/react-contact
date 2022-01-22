48a874ccdb6e13c5474af69eb2c80448ebb8dc75
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-01-22 17:31:50 +0100

    destructing props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
-function Contact(props) {
+function Contact({img, name, phone, email}) {
   return (
     <div className="contact-card">
-      <img src={props.img} alt={props.name} />
-      <h3>{props.name}</h3>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
       <div className="info-group">
-        <p>{props.phone}</p>
+        <p>{phone}</p>
       </div>
       <div className="info-group">
-        <p>{props.email}</p>
+        <p>{email}</p>
       </div>
     </div>
   );
